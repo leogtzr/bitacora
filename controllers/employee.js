a5b9a882212b44c4d3454045333b66ee721461b9
leogtzr <leogutierrezramirez@gmail.com>
2019-04-08 16:39:20 -0600

    Finishing code to add entries to employee

--- controllers/employee.js
+++ controllers/employee.js
@@ -9,10 +9,6 @@ var utils = require('../utils/utils');
 // Misc
 const apiMsg = 'Server Error.';
 
-function hello(req, res) {
-    res.status(200).send('Hello alrpvw');
-}
-
 function getAll(req, res) {
     Employee.find()
     .exec((err, employees) => {
@@ -34,19 +30,21 @@ function addEmployee(req, res) {
     employee.name = params.name;
     employee.nickname = params.nickname;
     
-    entry.when = params.entry.when;
-    entry.description = params.entry.description;
-    entry.notes = params.entry.notes;
-    employee.entries = entry;
+    if (params.entry) {
+        entry.when = params.entry.when;
+        entry.description = params.entry.description;
+        entry.notes = params.entry.notes;
+        employee.entries = entry;
+    }
 
-    if (employee.name && employee.entry) {
+    if (employee.name) {
 
         employee.save((err, newEmployee) => {
             if (err) {
                 res.status(500).send({message: apiMsg + ' ' + err});
             } else {
                 if (!newEmployee){
-                    res.status(404).send({message: "Error creating employee"});
+                    res.status(500).send({message: "Error creating employee"});
                 } else {
                     res.status(200).send(newEmployee);
                 }
@@ -58,8 +56,51 @@ function addEmployee(req, res) {
     }
 }
 
+function addEntryToEmployee(req, res) {
+
+    var params = req.body;
+    var entry = params.entry;
+
+    utils.printParams(req);
+
+    if (params.id) {
+        Employee.findById(params.id, (err, employee) => {
+            if (err) {
+                res.status(500).send({message: apiMsg + ' ' + err});
+            } else {
+
+                
+
+                var entry = new BitacoraEntry();
+                entry.when = params.entry.when;
+                entry.description = params.entry.description;
+                entry.notes = params.entry.notes;
+
+                entry.save((err, savedEntry) => {
+                    if (err) {
+                        res.status(500).send({message: apiMsg + ' ' + err});
+                    } else {
+                        employee.entries.push(savedEntry);
+                        employee.save((err, savedEmployee) => {
+                            if (err) {
+                                res.status(500).send({message: apiMsg + ' ' + err});
+                            } else {
+                                res.status(200).send(savedEmployee);
+                            }
+                        });
+
+                    }
+                });
+
+            }
+        });
+    } else {
+        res.status(400).send({message: 'Mising some fields ... '});
+    }
+}
+
 module.exports = {
-    hello,
     getAll,
-    addEmployee
+    addEmployee,
+    addEntryToEmployee
 };
--- routes/employee.js
+++ routes/employee.js
@@ -5,9 +5,8 @@ var api = express.Router();
 var controller = require('../controllers/employee');
 var mdAuth = require('../middlewares/authenticated');
 
-// CRUD Default
-api.get('/hello', controller.hello);
 api.get('/employees', controller.getAll);
 api.post('/employee', controller.addEmployee);
+api.post('/entrytoemp', controller.addEntryToEmployee);
 
 module.exports = api;
